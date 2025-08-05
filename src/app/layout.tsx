6822108f57a3210d7bda8ddd918d9650c78ca589
Giggles3237 <chris.lasko@gmail.com>
2025-08-04 22:08:55 -0400

    added heart tab icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,9 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: 'Daily Flirt',
   description: 'Daily flirty comments for every occasion',
+  icons: {
+    icon: '/lips.svg',
+  },
 };
 
 export default function RootLayout({
